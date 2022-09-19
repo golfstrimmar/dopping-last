7bf0f3d1236616e4cf9f9a128bdb4e48b31dc14a
golfstrimmar <64115098+golfstrimmar@users.noreply.github.com>
2022-09-19 22:58:59 +0200

    222

--- dist/bundle.js
+++ dist/bundle.js
@@ -7689,3 +7689,31 @@ const right = document.querySelectorAll("._enter-right");
 right.forEach((ent) => {
   observer3.observe(ent);
 });
+
+
+const MyLazy = () => {
+  let images = document.querySelectorAll("img ");
+  const options = {
+    root: null,
+    rootMargin: "-250px",
+    threshold: 0.1,
+  };
+  function handleImg(myImg, observer) {
+    myImg.forEach((myImgSingle) => {
+      // console.log(myImgSingle.intersectionRatio);
+      if (myImgSingle.intersectionRatio > 0) {
+        loadImg(myImgSingle.target);
+      }
+    });
+  }
+  function loadImg(img) {
+    img.src = img.getAttribute("data");
+  }
+
+  const observer = new IntersectionObserver(handleImg, options);
+  images.forEach((img) => {
+    observer.observe(img);
+  });
+};
+MyLazy();
+
